2abd2a9057cd3b63407589d435091104920f2a47
Daniel Romano <drom96@gmail.com>
2020-11-11 21:08:08 -0800

    Created basic GUI and started porting generation algos over to C++.
    Basic GUI allows for users to input scaling and generation features
    by manipulating scales and checkboxes. Utilizes slow method of
    writing image to a mapcanvas that will need to be changed at a later date.
    
    Moved bulk of perlin.py to c++ code for significant speed increaase. Was
    previously taking ~135 seconds to create a 1024x1024px image which has been
    shortened to .35 seconds thanks to C's optimizations! Perlin.py now contains
    the python bindings to call the method in c implemented using built in
    ctypes.

---
+++ perlin/Perlin.cpp
@@ -0,0 +1,108 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <stdint.h>
+#include <math.h>
+#include <chrono>
+
+using namespace std;
+
+
+void perlinNoise(int, double, double, double, double, double, int, uint8_t*, size_t);
+double perlinHelper(double, double, double, double, double, double, int, uint8_t*);
+double perlin(double, double, uint8_t*);
+double fade(double);
+double lerp(double, double, double);
+double grad(int, double, double);
+
+// Readable through .so via python.
+extern "C"{
+	double* perlinNoise(int size, double scale, double f, double l, double a, double p, int o, uint8_t *perms, double* grid)
+	{
+			// Width and height
+			int w = size;
+			int h = size;
+	
+			// Create noise value for everything in the grid
+			for(int y=0; y < h; y++)
+			{
+				for(int x = 0; x < w; x++)
+				{
+					grid[(y*w) + x] = perlinHelper((double)x/w/(double)scale, (double)y/h/(double)scale, f, l, a, p ,o, perms);
+				}
+			}
+
+			return grid;
+	}
+}
+
+
+double perlinHelper(double x, double y, double f, double l, double a, double p, int o, uint8_t*perms)
+{
+	double noiseVal = 0;
+	for(int i=0; i < o; i++){
+		noiseVal = noiseVal + (perlin(x*f, y*f, perms)*a);
+		a = a*p;
+		f = f * l;
+	}
+	return noiseVal/o;
+}
+
+
+double perlin(double x, double y, uint8_t* p)
+{
+	int cX = (int)floor(x) & 0xFF;
+	int cY = (int)floor(y) & 0xFF;
+
+	double pX = x - floor(x);
+	double pY = y - floor(y);
+
+	int g1 = (int)p[((int)p[cX] + cY) % 256];
+	int g2 = (int)p[((int)p[cX+1] + cY) % 256];
+	int g3 = (int)p[((int)p[cX] + cY+1) % 256];
+	int g4 = (int)p[((int)p[cX+1] + cY+1) % 256];
+
+	
+	double d1 = grad(g1, pX, pY);
+	double d2 = grad(g2, pX-1, pY);
+	double d3 = grad(g3, pX, pY-1);
+	double d4 = grad(g4, pX-1, pY-1);
+
+	double u = fade(pX);
+	double v = fade(pY);
+
+	double ret =  lerp(v, lerp(u, d1, d2), lerp(u, d3, d4));
+	return ret;
+}
+
+
+double fade(double t){
+	return t * t * t * (t * (t * 6 - 15) + 10);
+}
+
+
+double lerp(double t, double a, double b){
+	return a + (double)t * (b - a);
+	
+}
+
+
+double grad(int permHash, double x, double y)
+{
+	int gradDir = permHash & 3;
+
+	switch(gradDir){
+		case(0):
+			return x + y;
+		case(1):
+			return -x + y;
+		case(2):
+			return x - y;
+		case (3):
+			return -x - y;
+		default:
+			return 0;
+	}
+}
+
+
--- perlin/Perlin.py
+++ perlin/Perlin.py
@@ -3,70 +3,93 @@
 	improved perlin noise. Found at: https://mrl.cs.nyu.edu/~perlin/noise/
 '''
 import numpy as np
+from numpy.ctypeslib import ndpointer
 import math
 
-def PerlinNoise(shape:(int,int), scale:int = 1, f:int = 10, l:float = 2.0, a:float = 10.0, p:float = .5, o:int = 4, randPerm: bool = False):
+import ctypes
+import pathlib
+
+def PerlinNoise(shape:(int,int), scale:float = 1, f:float = 10, l:float = 2.0, a:float = 10.0, p:float = .5, o:int = 4, randPerm: bool = False, seed = None):
 	''' 
 		Creates a heightmap of the requestion shape. Permutation table used in generation
 		will either be Perlin's original table or a randomly generated one based on user request.
 		Noise is generated using the current position, scale and octave (f) for each
 		pixel in the height map. Scale determines how zoomed in or out the generated map appears.
 	'''
-	w,h = shape
-	perm = getPermTable(randPerm)
-	grid = np.zeros(shape)
-	
-	# Create noise value for each point given scale and F.L.A.P.O (Hehe, flapo).
-	for y in range(h):
-		for x in range(w):
-			grid[y][x] = _noise((x/ w/scale), (y/ h/scale), f, l, a, p, o, perm)
-	return grid
+	h,w = shape
+	# Get name
+	libname = pathlib.Path().absolute() / "perlin/perlin.so"
+	c_lib = ctypes.CDLL(libname)
 
+	c_lib.perlinNoise.argtypes = [ctypes.c_int, ctypes.c_double,
+								 ctypes.c_double, ctypes.c_double,
+								 ctypes.c_double, ctypes.c_double, 
+								 ctypes.c_int, ndpointer(ctypes.c_int8), ndpointer(ctypes.c_double)]
+	
+	c_lib.perlinNoise.restype = None
+	
+	perm = getPermTable(randPerm, seed)
+	grid = np.empty((h,w))
+
+	c_lib.perlinNoise(ctypes.c_int(h), ctypes.c_double(scale), 
+							ctypes.c_double(f), ctypes.c_double(l),
+							ctypes.c_double(a), ctypes.c_double(p),
+							ctypes.c_int(o), perm, grid)
+
+	return grid
+	
 
 def getPermTable(randPerm):
 	# Generate random permutation or use Ken Perlin's perm table.
+	'''
 	if randPerm:
+		# Doesn't allow selection of data types making it useless when using c :(
 		p = np.random.choice(256, 256, replace=False)
 	else:
-		'''
-			Obtained from Ken Perlin's Documentation. Instead of copying the array again
-			to fill with 512 values  I will use mod 256 when needed.
-		'''	
-		p = np.array([151,160,137,91,90,15,
-		131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,
-		190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
-	    88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
-		77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
-		102,143,54, 65,25,63,161, 1,216,80,73,209,76,132,187,208, 89,18,169,200,196,
-		135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,
-		5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
-		223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,
-		129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,
-		251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
-		49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
-		138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180], dtype=np.uint8)
+	'''
+	'''
+		Obtained from Ken Perlin's Documentation. Instead of copying the array again
+		to fill with 512 values  I will use mod 256 when needed.
+	'''	
+	p = np.array([151,160,137,91,90,15,
+	131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,
+	190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
+    88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
+	77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
+	102,143,54, 65,25,63,161, 1,216,80,73,209,76,132,187,208, 89,18,169,200,196,
+	135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,
+	5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
+	223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,
+	129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,
+	251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
+	49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
+	138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180], dtype=ctypes.c_int8)
+
+	# Add seed if given or clear seed.
+	np.random.seed(seed) if seed else np.random.seed()
+	if randPerm: np.random.shuffle(p)
 
 	return p
 
 
+# Code ported to c for speed.
 def fade(t):
 	'''
 		Allows for smoother transition between values.
 	'''
-	return t * t * t * (t * (t * 6 - 15) + 10) 
+	return  t * t * t * (t * (t * 6 - 15) + 10) 
 
 
 def lerp(t, a, b):
 	''' 
 		Perform linear interpolation between A and B.
-	'''
+	'''	
 	return a + t * (b - a)
 
 
 def grad(perm, x, y):
 	# Get one of the 4 possible gradient vectors
 	gradDir = perm & 3
-
 	if gradDir == 0:
 		return x + y
 	elif gradDir  == 1:
@@ -130,6 +153,7 @@ def noise(x, y, p):
 	g3 = p[(p[cX] + cY + 1) % 256]		# Bottom Left Corner
 	g4 = p[(p[(cX+1)%256] + cY +1) % 256]	# Bottom Right Corner
 
+
 	# Get distances
 	d1 = grad(g1, pX, pY)
 	d2 = grad(g2, pX - 1, pY)
@@ -137,6 +161,8 @@ def noise(x, y, p):
 	d4 = grad(g4, pX - 1, pY - 1)
 
 	u = fade(pX)
+
 	v = fade(pY)
+
 	
 	return lerp(v, lerp(u, d1, d2), lerp(u, d3,d4))
